e71218e7156a5bf1420f2993d4313fa6d812bb14
haseeb2412 <haseeb241202@gmail.com>
2024-02-29 22:10:58 +0500

    working

--- parenthesis_Stack/parenthesis_using_stack.c
+++ parenthesis_Stack/parenthesis_using_stack.c
@@ -1,55 +1,23 @@
-#include<stdio.h>
-#include<stdlib.h>
-
-
+#include <stdio.h>
+#include <stdlib.h>
+ 
 struct stack
 {
     int size;
     int top;
     char *arr;
 };
-
-int isEmpty(struct stack * ptr){
+ 
+int isEmpty(struct stack *ptr)
+{
     if (ptr->top == -1)
     {
-        return -1;
-    }else{
+        return 1;
+    }
+    else
+    {
         return 0;
     }
-    
-}
-
-
-
-//  push the element in the stack 
-
-void push(struct stack * ptr,char val){
-        if (isFull(ptr))
-        {
-            printf("stack overflow");
-        }else{
-            ptr->top++;
-            ptr->arr[ptr->top]=val;
-        }
-        
-}
-
-
-
-
-//  pop the element in the stack
-
-char pop(struct stack * ptr){
-        if (isEmpty(ptr))
-        {
-            printf("under underflow");
-            return -1;
-        }else{
-            char val = ptr->arr[ptr->top];
-            ptr->top--;
-            return val;
-        }
-        
 }
  
 int isFull(struct stack *ptr)
@@ -64,56 +32,65 @@ int isFull(struct stack *ptr)
     }
 }
  
-int parenthesisMatch(char * exp){
-    struct stack *sp;
-    sp->size =100;
-     sp->top =-1;
-    sp->arr = (char *)malloc(sp->size*sizeof(char));
-
-    for (int i = 0; i < exp[i]== '\0'; i++)
-    {
-        if (exp[i]=='(')
-        {
-            push(sp,'(');
-            
-        }else if(exp[i]==')'){
-            if (isEmpty(sp))
-            {
-                return 0;
-            }
-            pop(sp);
-            
-        }
-        
-    }
-
-    if (isEmpty(sp))
-    {
-        return 1;
-    }else{
-        return 0;
-    }
-    
-}
-
-
-int main(){
-    // struct stack *s = malloc(sizeof(struct stack));
-    // s->size=10;
-    // s->top =-1;
-    // s->arr = (char *)malloc(s->size*sizeof(char));
-    // printf("parenthesis Using stavk");
-    // return 0;
-
-
-    char * exp = "8*(9)";
-    if (parenthesisMatch(exp))
-    {
-        printf("parenthesis is matched \n");
+void push(struct stack* ptr, char val){
+    if(isFull(ptr)){
+        printf("Stack Overflow! Cannot push %c to the stack\n", val);
     }
     else{
-        printf("parenthesis is not matched \n");
-
+        ptr->top++;
+        ptr->arr[ptr->top] = val;
     }
-    
+}
+ 
+char pop(struct stack* ptr){
+    if(isEmpty(ptr)){
+        printf("Stack Underflow! Cannot pop from the stack\n");
+        return -1;
+    }
+    else{
+        char val = ptr->arr[ptr->top];
+        ptr->top--;
+        return val;
+    }
+}
+ 
+int parenthesisMatch(char * exp){
+    // Create and initialize the stack
+    struct stack* sp = (struct stack*)malloc(sizeof(struct stack));
+    sp->size = 100;
+    sp->top = -1;
+    sp->arr = (char *)malloc(sp->size * sizeof(char));
+ 
+    for (int i = 0; exp[i]!='\0'; i++)
+    {
+        if(exp[i]=='('){
+            push(sp, '(');
+        }
+        else if(exp[i]==')'){
+            if(isEmpty(sp)){
+                free(sp->arr); // Free memory before returning
+                free(sp);
+                return 0;
+            }
+            pop(sp); 
+        }
+    }
+ 
+    int result = isEmpty(sp);
+    free(sp->arr); // Free memory before returning
+    free(sp);
+    return result;
+}
+ 
+int main()
+{
+    char * exp = "((8)(*--$$9)";
+    // Check if stack is empty
+    if(parenthesisMatch(exp)){
+        printf("The parenthesis is matching\n");
+    }
+    else{
+        printf("The parenthesis is not matching\n");
+    }
+    return 0;
 }
