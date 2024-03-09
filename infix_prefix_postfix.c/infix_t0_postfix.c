7a448d3dc1c07aa3167762753d615ae6bfe7cea9
haseeb2412 <haseeb241202@gmail.com>
2024-03-09 22:33:18 +0500

    working

--- infix_prefix_postfix.c/infix_t0_postfix.c
+++ infix_prefix_postfix.c/infix_t0_postfix.c
@@ -129,14 +129,19 @@ char * infixToPostfix(char *infix){
 
 }
  
-
-
-
-
-
 int main()
 {
     char * infix = "x-y/z-k*d";
     printf("postfix is %s\n", infixToPostfix(infix));
     return 0;
 }
+
+
+
+
+// int main()
+// {
+//     char * infix = "x-y/z-k*d";
+//     printf("postfix is %s\n", infixToPostfix(infix));
+//     return 0;
+// }
