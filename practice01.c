977f2296a0fb5f824db8ea76ef62324a6bd6aa50
haseeb2412 <haseeb241202@gmail.com>
2024-03-16 16:22:14 +0500

    prctice linked list

--- practice01.c
+++ practice01.c
@@ -1,3 +1,156 @@
+// #include<stdio.h>
+// #include<stdlib.h>
+
+// struct Node
+// {
+//     int data;
+//     struct Node * next;
+// };
+
+// // linked list traversal function
+
+// void linkedListTraversal(struct Node * ptr){
+//     while (ptr != NULL)
+//     {
+//         printf("Element is :%d\n",ptr->data);
+//         ptr =ptr->next;
+//     }
+    
+// }
+
+// // delte the first node in the linked list 
+ 
+// struct Node * deleteFirst(struct Node * head){
+//     struct Node * ptr =head;
+//     head = head->next;
+//     free(ptr);
+//     return head;
+// }
+
+// // delete the last Node in the linked list 
+
+// struct Node * deleteAtEnd(struct Node * head){
+//     struct Node * p =head;
+//     struct Node * q =head->next;
+
+//     while (q->next != NULL)
+//     {
+//          p =q;
+//          q = q->next;
+//     }
+//     p->next = NULL;
+//     free(q);
+//     return head;
+    
+// }
+
+// //delete the node at specific index in the linked list 
+
+// struct Node * deleteAtNode(struct Node * head,int value){
+//     struct Node * p =head;
+//     struct Node * q =head->next;
+    
+//     if(head == value){
+//         head=p->next;
+//         free(p);
+//         return head;
+//     }
+    
+//      while (q != NULL && q->data != value) {
+//         p = p->next;
+//         q = q->next;
+//     }
+
+//     // If the node with the given value is found
+//     if (q != NULL) {
+//         p->next = q->next;
+//         free(q);
+//     }
+//     return head;
+    
+// }
+// int main(){
+
+//     // initilize node  in heap
+
+//     struct Node * head=(struct Node *)malloc(sizeof(struct Node));
+//     struct Node * second=(struct Node *)malloc(sizeof(struct Node));
+//     struct Node * third=(struct Node *)malloc(sizeof(struct Node));
+//     struct Node * four=(struct Node *)malloc(sizeof(struct Node));
+//     struct Node * five=(struct Node *)malloc(sizeof(struct Node));
+
+//     // connect one Node to another 
+//     head->data=10;
+//     head->next=second;
+
+//     second->data=20;
+//     second->next=third;
+
+//     third->data=30;
+//     third->next=four;
+
+//     four->data=40;
+//     four->next=five;
+
+//     five->data=50;
+//     five->next=NULL;
+
+
+//     // -----------------CASE 01 -------------------
+
+//     printf("-----Before-----\n");
+//     linkedListTraversal(head);
+
+//     printf("-----After-----\n");
+//     head=deleteFirst(head);
+
+//     linkedListTraversal(head);
+
+
+//     // -----------------CASE 02 -------------------
+
+//      printf("-----Before-----\n");
+//     linkedListTraversal(head);
+
+//     printf("-----After-----\n");
+//     head=deleteAtEnd(head);
+
+//     linkedListTraversal(head);
+
+//     // -----------------CASE 03 -------------------
+
+
+//        printf("-----Before-----\n");
+//     linkedListTraversal(head);
+
+//     printf("-----After-----\n");
+//     head=deleteAtSpecific(head,2);
+
+//     linkedListTraversal(head);
+
+//     // -----------------CASE 04 -------------------
+    
+//     printf("-----Before-----\n");
+//     linkedListTraversal(head);
+
+//     printf("-----After-----\n");
+//     head=deleteAtNode(head,2);
+
+//     linkedListTraversal(head);
+
+//     return 0;
+// }
+
+
+
+
+
+
+
+
+//  -------------------------------------------------INSERTION LINKED LIST --------------------------------------------------------
+
+
 #include<stdio.h>
 #include<stdlib.h>
 
@@ -7,135 +160,47 @@ struct Node
     struct Node * next;
 };
 
-// linked list traversal function
+// linked list traversal 
 
 void linkedListTraversal(struct Node * ptr){
     while (ptr != NULL)
     {
         printf("Element is :%d\n",ptr->data);
-        ptr =ptr->next;
+        ptr = ptr->next;
     }
     
 }
 
-// delte the first node in the linked list 
- 
-struct Node * deleteFirst(struct Node * head){
-    struct Node * ptr =head;
-    head = head->next;
-    free(ptr);
-    return head;
-}
-
-// delete the last Node in the linked list 
-
-struct Node * deleteAtEnd(struct Node * head){
-    struct Node * p =head;
-    struct Node * q =head->next;
-
-    while (q->next != NULL)
-    {
-         p =q;
-         q = q->next;
-    }
-    p->next = NULL;
-    free(q);
-    return head;
-    
-}
-
-//delete the node at specific index in the linked list 
-
-struct Node * deleteAtNode(struct Node * head,int value){
-    struct Node * p =head;
-    struct Node * q =head->next;
-    
-    if(head == value){
-        head=p->next;
-        free(p);
-        return head;
-    }
-    
-     while (q != NULL && q->data != value) {
-        p = p->next;
-        q = q->next;
-    }
-
-    // If the node with the given value is found
-    if (q != NULL) {
-        p->next = q->next;
-        free(q);
-    }
-    return head;
-    
-}
 int main(){
-
-    // initilize node  in heap
-
-    struct Node * head=(struct Node *)malloc(sizeof(struct Node));
-    struct Node * second=(struct Node *)malloc(sizeof(struct Node));
-    struct Node * third=(struct Node *)malloc(sizeof(struct Node));
-    struct Node * four=(struct Node *)malloc(sizeof(struct Node));
-    struct Node * five=(struct Node *)malloc(sizeof(struct Node));
-
-    // connect one Node to another 
-    head->data=10;
-    head->next=second;
-
-    second->data=20;
-    second->next=third;
-
-    third->data=30;
-    third->next=four;
-
-    four->data=40;
-    four->next=five;
-
-    five->data=50;
-    five->next=NULL;
+    struct Node * head = (struct Node *)malloc(sizeof(struct Node));
+    struct Node * second = (struct Node *)malloc(sizeof(struct Node));
+    struct Node * third = (struct Node *)malloc(sizeof(struct Node));
+    struct Node * four = (struct Node *)malloc(sizeof(struct Node));
+    struct Node * five = (struct Node *)malloc(sizeof(struct Node));
+    struct Node * sixth = (struct Node *)malloc(sizeof(struct Node));
 
 
-    // -----------------CASE 01 -------------------
+    // connecting Nodes in linked list 
 
-    // printf("-----Before-----\n");
-    // linkedListTraversal(head);
+    head->data = 10;
+    head->next =second;
 
-    // printf("-----After-----\n");
-    // head=deleteFirst(head);
+    second->data = 20;
+    second->next =third;
 
-    // linkedListTraversal(head);
+    third->data = 30;
+    third->next =four;
 
+    four->data = 40;
+    four->next =five;
 
-    // -----------------CASE 02 -------------------
+    five->data = 50;
+    five->next =sixth;
 
-    //  printf("-----Before-----\n");
-    // linkedListTraversal(head);
+    sixth->data = 60;
+    sixth->next =NULL;
 
-    // printf("-----After-----\n");
-    // head=deleteAtEnd(head);
-
-    // linkedListTraversal(head);
-
-    // -----------------CASE 03 -------------------
-
-
-    //    printf("-----Before-----\n");
-    // linkedListTraversal(head);
-
-    // printf("-----After-----\n");
-    // head=deleteAtSpecific(head,2);
-
-    // linkedListTraversal(head);
-
-    // -----------------CASE 04 -------------------
-    
-    printf("-----Before-----\n");
-    linkedListTraversal(head);
-
-    printf("-----After-----\n");
-    head=deleteAtNode(head,2);
-
+    printf("Before\n");
     linkedListTraversal(head);
 
     return 0;
